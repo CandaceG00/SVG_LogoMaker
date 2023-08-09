7b8c57715c5452a8709da99e3ac8a960e93aac35
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-08-09 15:49:52 -0400

    added logo.svg and determined basic shapes.js classes

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {Triange, Square, Circle} = require('./library/shapes');
+
+
+function promptUser() {
+    inquirer
+      .prompt([
+        // Text prompt
+        {
+          type: "input",
+          message:
+            "What text would you like you logo to display? (Enter up to three characters)",
+          name: "text",
+        },
+        // Text color prompt
+        {
+          type: "input",
+          message:
+            "Choose text color (Enter color keyword OR a hexadecimal number)",
+          name: "textColor",
+        },
+        // Shape choice prompt
+        {
+          type: "list",
+          message: "What shape would you like the logo to render?",
+          choices: ["Triangle", "Square", "Circle"],
+          name: "shape",
+        },
+        // Shape color prompt
+        {
+          type: "input",
+          message:
+            "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+          name: "shapeBackgroundColor",
+        },
+      ])
+      .then((answers) => {
+        // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+        if (answers.text.length > 3) {
+          console.log("Must enter a value of no more than 3 characters");
+          promptUser();
+        } else {
+          // Calling write file function to generate SVG file
+          writeToFile("logo.svg", answers);
+        }
+      });
+  }
+  promptUser();

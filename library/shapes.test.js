f6574d1f4e18139ee776c86f7dcda53d53f22839
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-08-13 16:54:26 -0400

    modified tests to include color

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -3,19 +3,19 @@ const { Triangle, Square, Circle } = require('./shapes');
 describe('shape', () => {
     test('Triangle should render correctly', () => {
         const triangle = new Triangle();
-        const expected = '<polygon points="150, 18 244, 182 56, 182" fill="' + triangle.color + '" />';
-        expect(triangle.render()).toBe(expected);
+        triangle.setColor("blue");
+        expect(triangle.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
     });
 
     test('Square should render correctly', () => {
         const square = new Square();
-        const expected = '<rect x="73" y="40" width="160" height="160" fill="' + square.color + '" />';
-        expect(square.render()).toBe(expected);
+        square.setColor("red");
+        expect(square.render()).toEqual('<rect x="73" y="40" width="160" height="160" fill="red" />');
     });
 
     test('Circle should render correctly', () => {
         const circle = new Circle();
-        const expected = '<circle cx="150" cy="115" r="80" fill="' + circle.color + '" />';
-        expect(circle.render()).toBe(expected);
+        circle.setColor("green");
+        expect(circle.render()).toEqual('<circle cx="150" cy="115" r="80" fill="green" />');
     });
 })
